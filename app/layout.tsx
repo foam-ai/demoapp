932e8e124946f61c0d0a9725c2ead871970716dc
pcga11 <perlagamez@berkeley.edu>
2024-07-23 15:48:38 -0700

    some nits

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from 'react';
 import { Inter } from 'next/font/google';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
 import Header from '@/app/components/Header';
 import Modal from '@/app/components/Modal';
 import Footer from '@/app/components/Footer';
@@ -16,28 +15,26 @@ const inter = Inter({ subsets: ['latin'] });
 function RootLayoutContent({ children }: { children: React.ReactNode }) {
   const [isContactModalOpen, setIsContactModalOpen] = useState(false);
   const { applicationMap } = useProductContext();
-  
+
   const openContactModal = () => setIsContactModalOpen(true);
   const closeContactModal = () => setIsContactModalOpen(false);
 
   return (
     <html lang="en" className={inter.className}>
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <link
           rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         />
       </head>
       <body>
-        <UserProvider>
-          <Header openContactModal={openContactModal} applicationMap={applicationMap} />
-          <Modal isOpen={isContactModalOpen} onClose={closeContactModal}>
-            <ContactUs onClose={closeContactModal} />
-          </Modal>
-          {children}
-          <Footer />
-        </UserProvider>
+        <Header openContactModal={openContactModal} applicationMap={applicationMap} />
+        <Modal isOpen={isContactModalOpen} onClose={closeContactModal}>
+          <ContactUs onClose={closeContactModal} />
+        </Modal>
+        {children}
+        <Footer />
       </body>
     </html>
   );
--- app/requests/page.tsx
+++ app/requests/page.tsx
@@ -1,39 +1,33 @@
 'use client';
 
-import { useUser } from '@auth0/nextjs-auth0/client';
 import { useEffect, useState, FC } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
 import { Request } from '@/app/types';
 
 const Requests: FC = () => {
-  const { user, error, isLoading } = useUser();
   const [requests, setRequests] = useState<Request[]>([]);
   const [fetchError, setFetchError] = useState<string | null>(null);
 
   const fetchRequests = async () => {
-    if (user) {
-      const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-      try {
-        const response = await axios.get(`${backendUrl}/get_requests`, {
-          params: { user_id: user.sub }
-        });
+    try {
+      const response = await axios.get(`${backendUrl}/get_requests`);
 
-        if (response.status === 200) {
-          setRequests(response.data);
-        } else {
-          setFetchError('Failed to fetch requests');
-        }
-      } catch (error) {
-        setFetchError('Something went wrong while fetching requests');
+      if (response.status === 200) {
+        setRequests(response.data);
+      } else {
+        setFetchError('Failed to fetch requests');
       }
+    } catch (error) {
+      setFetchError('Something went wrong while fetching requests');
     }
   };
 
   useEffect(() => {
     fetchRequests();
-  }, [user]);
+  }, []);
 
   const handleCreateQuotes = async (requestId: string, query: string) => {
     try {
@@ -47,8 +41,7 @@ const Requests: FC = () => {
     }
   };
 
-  if (isLoading) return <div className="flex justify-center items-center h-screen text-black">Loading...</div>;
-  if (error || fetchError) return <div className="flex justify-center items-center h-screen text-red-500">{error?.message || fetchError}</div>;
+  if (fetchError) return <div className="flex justify-center items-center h-screen text-red-500">{fetchError}</div>;
 
   return (
     <div className="container mx-auto px-4 py-8 text-black">
@@ -66,14 +59,14 @@ const Requests: FC = () => {
         <tbody>
           {requests.map((request) => (
             <tr key={request.request_id} className="text-black">
-              <td className="py-2 px-4 border-b"> {request.product_name}</td>
+              <td className="py-2 px-4 border-b">{request.product_name}</td>
               <td className="py-2 px-4 border-b">{request.description}</td>
               <td className="py-2 px-4 border-b">{request.status.toUpperCase()}</td>
               <td className="py-2 px-4 border-b">{new Date(request.created_at * 1000).toLocaleString()}</td>
               <td className="py-2 px-4 border-b">
                 {request.status === 'COMPLETED' ? (
-                  <Link className="text-blue-500 hover:underline"  href={`/quotes/${request.request_id}`}>
-                      See Quotes
+                  <Link className="text-blue-500 hover:underline" href={`/quotes/${request.request_id}`}>
+                    See Quotes
                   </Link>
                 ) : (
                   <button
