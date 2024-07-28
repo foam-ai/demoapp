b7c4b8a7af91f169ab3e7ea48e8af149f2a58ae5
pcga11 <perlagamez@berkeley.edu>
2024-07-28 14:45:04 -0700

    Clean up

--- frontend/app/routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .contactus import router as contactus_router
-from .careers import router as careers_router
-from .github_webhook import router as github_webhook_router
-from .root_cause import router as root_cause_router
