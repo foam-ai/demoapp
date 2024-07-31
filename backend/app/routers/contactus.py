2208171f155f6d3e0af5d4fe5fc0876fb368ae1f
pcga11 <perlagamez@berkeley.edu>
2024-07-31 10:17:36 -0700

    Undo bug; that was a test

--- backend/app/routers/contactus.py
+++ backend/app/routers/contactus.py
@@ -24,8 +24,7 @@ async def submit_contact(submission: ContactSubmission):
     try:
         result = typesenseClient.collections['contactus'].documents.create(contact['data'])
         logger.info(contact)
-        # return {"message": "Contact us form submitted successfully", "data": result}
-        raise Exception("oopse")
+        return {"message": "Contact us form submitted successfully", "data": result}
     except Exception as exc:
         contact["status"] = "error"
         contact["error"] = str(exc)
