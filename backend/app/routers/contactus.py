2a7215e1f7abec585fcca2be8a427ec6d7dc2425
Perla Gamez <pcga11@users.noreply.github.com>
2024-08-28 17:13:45 -0700

    Update contactus.py

--- backend/app/routers/contactus.py
+++ backend/app/routers/contactus.py
@@ -22,9 +22,8 @@ async def submit_contact(submission: ContactSubmission):
         }
     }
     try:
-        logger.info(contact)
         result = typesenseClient.collections['contactus'].documents.create(contact['data'])
-        logger.info("Yay it worked.")
+        logger.info(contact)
         return {"message": "Contact us form submitted successfully", "data": result}
     except Exception as exc:
         contact["status"] = "error"
