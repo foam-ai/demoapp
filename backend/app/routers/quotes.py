f900ee836ff1d4eae40a11b3c768cac3bd184dc2
pcga11 <perlagamez@berkeley.edu>
2024-11-09 15:08:48 -0800

    Clecn up

--- backend/app/routers/quotes.py
+++ backend/app/routers/quotes.py
@@ -1,23 +1,13 @@
-from fastapi import APIRouter, HTTPException
-# from app.utils.typesense_client import typesenseClient
+from fastapi import APIRouter
+from app.utils.typesense_client import typesenseClient
 from app.utils.models import QuoteSubmission
-from app.utils.logger import logger
 from datetime import datetime
-import requests
-import os
-import time
-from dotenv import load_dotenv
-
-load_dotenv()
-
-third_party_client_host_url = os.getenv('THIRD_PARTY_CLIENT_HOST_URL')
 
 router = APIRouter()
 
 
 @router.post("/quotes")
 async def submit_quote(submission: QuoteSubmission):
-    endpoint_start_time = time.time()
     endpoint = "/quotes"
     quote = {
         "endpoint": endpoint,
@@ -34,33 +24,5 @@ async def submit_quote(submission: QuoteSubmission):
             "created_at": int(datetime.utcnow().timestamp())
         }
     }
-    try:
-        # result = typesenseClient.collections['quotes'].documents.create(quote['data'])
-        result = "blah"
-        endpoint_end_time = time.time()
-        total_latency = (endpoint_end_time - endpoint_start_time) * 1000  # Convert to milliseconds
-        quote["total_endpoint_latency"] = f"{total_latency:.2f}ms"
-
-        logger.info(quote)
-        return {"message": "Quote request submitted successfully", "data": result}
-    except ValueError as ve:
-        quote["status"] = "error"
-        quote["error"] = f"Configuration error: {str(ve)}"
-        logger.error(quote)
-        raise HTTPException(status_code=500, detail=str(ve))
-    except requests.RequestException as req_exc:
-        quote["status"] = "error"
-        quote["error"] = f"Ownership endpoint error: {str(req_exc)}"
-        logger.error(quote)
-        raise HTTPException(status_code=500, detail=str(req_exc))
-    except Exception as exc:
-        quote["status"] = "error"
-        quote["error"] = str(exc)
-        logger.error(quote)
-        raise HTTPException(status_code=500, detail=str(exc))
-    finally:
-        if "total_endpoint_latency" not in quote:
-            endpoint_end_time = time.time()
-            total_latency = (endpoint_end_time - endpoint_start_time) * 1000
-            quote["total_endpoint_latency"] = f"{total_latency:.2f}ms"
-        logger.info(quote)
+    result = typesenseClient.collections['quotes'].documents.create(quote['data'])
+    return {"message": "Quote request submitted successfully", "data": result}
