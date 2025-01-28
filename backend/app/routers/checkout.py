47512153e26a93817640a23962fd7388571b2e05
pcga11 <perla@foam.ai>
2025-01-28 15:36:21 -0800

    Harder problem

--- backend/app/routers/checkout.py
+++ backend/app/routers/checkout.py
@@ -1,38 +1,11 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
-from typing import Optional
 from app.utils.typesense_client import typesenseClient
 from datetime import datetime
 
 router = APIRouter()
- 
-class CheckoutSubmission(BaseModel):
-    cardNumber: str
-    cvc: str
-    zipCode: str
-    firstName: str
-    lastName: str
-    shippingStreet: str
-    shippingCity: str
-    shippingState: str
-    shippingZipCode: str
-    shippingMethod: str
 
 @router.post("/checkout")
-async def submit_checkout(submission: CheckoutSubmission):
-    checkout = {
-        "cardNumber": submission.cardNumber,
-        "cvc": submission.cvc,
-        "zipCode": submission.zipCode,
-        "firstName": submission.firstName,
-        "lastName": submission.lastName,
-        "shippingStreet": submission.shippingStreet,
-        "shippingCity": submission.shippingCity,
-        "shippingState": submission.shippingState,
-        "shippingZipCode": submission.shippingZipCode,
-        "shippingMethod": submission.shippingMethod,
-        "created_at": int(datetime.utcnow().timestamp())
-    }
-        
+async def submit_checkout(checkout: dict):
+    checkout["created_at"] = int(datetime.utcnow().timestamp())
     result = typesenseClient.collections['checkout'].documents.create(checkout)
     return {"message": "Checkout request submitted successfully", "data": result}
