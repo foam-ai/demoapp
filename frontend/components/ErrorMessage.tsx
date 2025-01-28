9e1d53aa3fba17ed9acc14bf07c1546148581876
pcga11 <perla@foam.ai>
2025-01-28 15:43:40 -0800

    Fixes

--- frontend/components/ErrorMessage.tsx
+++ frontend/components/ErrorMessage.tsx
@@ -7,8 +7,7 @@ interface ErrorMessageProps {
       <div className="rounded-md bg-red-50 p-4 mb-6">
         <div className="flex">
           <div className="ml-3">
-            <h3 className="text-sm font-medium text-red-800">Error</h3>
-            <div className="mt-2 text-sm text-red-700">
+            <div className="mt-2 text-sm text-center text-red-700">
               {message}
             </div>
           </div>
