87f4c60a8d3d0b6f83197cba9a28878165819a48
pcga11 <perla@foam.ai>
2025-01-28 15:23:53 -0800

    Add advanced demo

--- backend/app/utils/typesense_client.py
+++ backend/app/utils/typesense_client.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 # Read Typesense configuration from environment variables
-typesense_host = "host"
+typesense_host = os.getenv('TYPESENSE_HOST') or "host"
 typesense_port = os.getenv('TYPESENSE_PORT') or "443"
 typesense_protocol = os.getenv('TYPESENSE_PROTOCOL') or "https"
 typesense_api_key = os.getenv('TYPESENSE_ADMIN_API_KEY') or ""
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,35 +1,60 @@
 'use client'
+
 import { useState } from 'react'
 import CheckoutForm from '../components/CheckoutForm'
 import ErrorMessage from '../components/ErrorMessage'
 import Image from 'next/image'
+import * as Sentry from '@sentry/node';
 
 export default function CheckoutPage() {
   const [error, setError] = useState<string>('')
 
-  const handleCheckout = async (cardNumber: string, zipCode: string) => {
+  const handleCheckout = async (formData: {
+    firstName: string
+    lastName: string
+    cardNumber: string
+    cvc: string
+    zipCode: string
+    shippingStreet: string
+    shippingCity: string
+    shippingState: string
+    shippingZipCode: string
+    shippingMethod: string
+  }) => {
     try {
       const response = await fetch('https://demoapp-y43d.onrender.com/checkout', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ cardnumber: cardNumber, zipcode: zipCode }),
+        body: JSON.stringify({
+          cardnumber: formData.cardNumber,
+          zipcode: formData.zipCode,
+          firstName: formData.firstName,
+          lastName: formData.lastName,
+          street: formData.shippingStreet,
+          city: formData.shippingCity,
+          state: formData.shippingState,
+          method: formData.shippingMethod
+        }),
       })
 
       if (response.ok) {
         window.location.href = '/success'
       } else {
+        const errorMessage = await response.json()
         setError('Sorry, something went wrong with your payment. Please try again.')
+        Sentry.captureException(errorMessage)
       }
     } catch (error) {
       setError('Sorry, something went wrong with your payment. Please try again.')
+      Sentry.captureException(error)
     }
   }
 
   return (
     <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md mx-auto bg-white rounded-lg shadow-md p-6">
+      <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-md p-8">
         <div className="text-center mb-8">
           <Image
             src="/tennis.png"
