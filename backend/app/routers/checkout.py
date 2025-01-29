b479f9fa7577f52136d9b4f03a00cd361edc79e6
pcga11 <perla@foam.ai>
2025-01-28 16:51:20 -0800

    s

--- backend/app/routers/checkout.py
+++ backend/app/routers/checkout.py
@@ -1,27 +1,11 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter
 from app.utils.typesense_client import typesenseClient
 from datetime import datetime
-from typing import Dict
-from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
 @router.post("/checkout")
-async def submit_checkout(checkout: Dict):
-    try:
-        checkout["created_at"] = int(datetime.utcnow().timestamp())
-        result = typesenseClient.collections['checkout'].documents.create(checkout)
-        
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={
-                "message": "Checkout request submitted successfully",
-                "data": result
-            }
-        )
-
-    except Exception as e:
-        return JSONResponse(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                content={"message": str(e.message)}
-            )
+async def submit_checkout(checkout: dict):
+    checkout["created_at"] = int(datetime.utcnow().timestamp())
+    result = typesenseClient.collections['checkout'].documents.create(checkout)
+    return {"message": "Checkout request submitted successfully", "data": result}
